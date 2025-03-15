f5a4bf8de4c9d5795722166f04caaa22c8281420
gustavosouto02 <gugasouto2004@gmail.com>
2025-03-15 11:06:16 -0300

    add

---
+++ atividade 03/script.js
@@ -0,0 +1,16 @@
+var contador = 0;
+var contagem = 0;
+
+document.getElementById("count").innerHTML = contador;
+
+function button1(){
+    document.getElementById("count").innerHTML = ++contador;
+}
+
+function button2(){
+    document.getElementById("count").innerHTML = --contador;
+}
+
+if (contagem = )
+
+
